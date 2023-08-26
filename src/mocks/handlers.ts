047a396717c8b075396c1fa0143f503c2a5ded7d
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-08-26 19:54:10 +0200

    Add mocks and handlers to test RobotsSlice component

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,8 @@
+import { rest } from "msw";
+import { robotsMock } from "./mocksData";
+
+export const handlers = [
+  rest.get(`${import.meta.env.VITE_API_ROBOTS_URL}robots`, (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(robotsMock));
+  }),
+];
---
+++ src/mocks/mocksData.ts
@@ -0,0 +1,19 @@
+import { ApiRobots } from "../types";
+
+export const robotsMock: ApiRobots[] = [
+  {
+    id: "1",
+    name: "words destructor",
+    image: "https://i.ytimg.com/vi/PFPIaU3ESOw/maxresdefault.jpg",
+    speed: 9,
+    resistance: 5,
+  },
+  {
+    id: "2",
+    name: "robot smasher",
+    image:
+      "https://amcnetworks.es/wp-content/uploads/2018/09/Battlebots_Blaze.jpg",
+    speed: 6,
+    resistance: 9,
+  },
+];
